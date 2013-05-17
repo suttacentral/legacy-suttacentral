f61785cb9365f9ce1ba1dc4c8afb4f1879d72820
John Nishinaga <jingoro@casa-z.org>
2013-05-17 14:58:30 -0700

    Add HTML5 Boilerplate goodness

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from jinja2 import Environment, FileSystemLoader
 
 BASE_PATH = os.path.realpath(os.path.dirname(__file__))
 CONFIG_PATH = os.path.join(BASE_PATH, 'app.conf')
-LOCALCONFIG_PATH = os.path.join(BASE_PATH, 'local.conf')
+LOCAL_CONFIG_PATH = os.path.join(BASE_PATH, 'local.conf')
+STATIC_BASE_PATH = os.path.join(BASE_PATH, 'static')
 
 #
 # Configuration
@@ -21,12 +22,18 @@ def deep_update(a, b):
 
 config = cherrypy.lib.reprconf.as_dict(CONFIG_PATH)
 try:
-    deep_update(config, cherrypy.lib.reprconf.as_dict(LOCALCONFIG_PATH))
+    deep_update(config, cherrypy.lib.reprconf.as_dict(LOCAL_CONFIG_PATH))
 except IOError:
     pass
-deep_update(config, {'/': {
-    'tools.staticdir.root': os.path.join(BASE_PATH, 'static')
-}})
+
+# Absolutize any relative paths that cherrypy can't handle.
+for key, subdict in config.items():
+    if key[0] == '/':
+        subkey = 'tools.staticfile.filename'
+        if subkey in subdict:
+            subdict[subkey] = os.path.join(STATIC_BASE_PATH, subdict[subkey])
+
+config['/']['tools.staticdir.root'] = STATIC_BASE_PATH
 
 cherrypy.engine.autoreload.files.add(os.path.join(BASE_PATH, 'app.conf'))
 cherrypy.engine.autoreload.files.add(os.path.join(BASE_PATH, 'local.conf'))
@@ -38,7 +45,15 @@ cherrypy.engine.autoreload.files.add(os.path.join(BASE_PATH, 'local.conf'))
 env = Environment(loader=FileSystemLoader('templates'))
 db = mysql.connector.connect(**config['db'])
 
+def error_page_404(status, message, traceback, version):
+    return open(os.path.join(STATIC_BASE_PATH, '404.html'), 'r').read()
+
 class App:
+
+    _cp_config = {
+        'error_page.404': error_page_404
+    }
+
     @cherrypy.expose
     def index(self):
         cursor = db.cursor()
