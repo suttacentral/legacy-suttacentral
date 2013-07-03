7df7652ff9333bfaba19820bb49105f6abc38c5b
John Nishinaga <jingoro@casa-z.org>
2013-07-03 11:46:17 -0700

    Clean up Config and add doc block

--- python/config.py
+++ python/config.py
@@ -1,14 +1,60 @@
 import os, sys
 import cherrypy
 
-class Config:
+class Config(dict):
+    """A flexible and convenient configuration class for SuttaCentral.
 
-    base_dir         = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    source_dir       = os.path.join(base_dir, 'python')
-    global_conf_path = os.path.join(base_dir, 'global.conf')
-    local_conf_path  = os.path.join(base_dir, 'local.conf')
+    To use it, add an entry in global.conf under the [app] section:
 
-    ABSOLUTIZE_KEYS = [
+        [app]
+            level: 123
+
+    Then, you can access it as follows:
+
+        >>> import config
+        >>> config.level
+        123
+
+    Any entries specified in local.conf will override those entries in
+    global.conf. This allows a local configuration to specify settings
+    without having those settings be put into the global code repository.
+
+    You can add any Python value, including arrays and dictionaries:
+
+        [app]
+            spain: {'color': 'purple'}
+
+        >>> config.spain
+        {'color': 'purple'}
+
+    Dictionaries can also be added to the top-level and accessed easily:
+
+        [brazil]
+            color: 'green'
+            sport: 'football'
+
+        >>> config.brazil
+        {'color': 'green', 'sport': 'football'}
+
+    More explicitly, Config's magic getattr looks for entries under 'app',
+    and if can't find it there, it then looks in the top-level.
+
+    The config object is a regular Python dict. You can still access entries
+    using the [] method:
+
+        >>> config['app']['level']
+        123
+        >>> config['brazil']['a']
+        1
+
+    Entries listed under ABSOLUTE_PATHS will be automatically converted into
+    absolute paths (e.g., required by cherrypy).
+
+    The config object itself is intended to be fed directly into cherrypy. See
+    server.py.
+    """
+
+    ABSOLUTE_PATHS = [
         ['global', 'access_file'],
         ['global', 'error_file'],
         ['app', 'app_log_file'],
@@ -19,39 +65,47 @@ class Config:
         ['sqlite', 'db'],
     ]
 
+    base_dir         = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    source_dir       = os.path.join(base_dir, 'python')
+    global_conf_path = os.path.join(base_dir, 'global.conf')
+    local_conf_path  = os.path.join(base_dir, 'local.conf')
+
     def __init__(self):
+        dict.__init__(self)
         self.__setup()
 
     def __getattr__(self, name):
-        if name in self.config['app']:
-            return self.config['app'][name]
-        elif name in self.config:
-            return self.config[name]
+        if 'app' in self and name in self['app']:
+            return self['app'][name]
+        elif name in self:
+            return self[name]
         else:
             raise AttributeError("Config has no attribute '%s'" % name)
 
     def __setup(self):
-        self.config = cherrypy.lib.reprconf.as_dict(self.global_conf_path)
+        config = cherrypy.lib.reprconf.as_dict(self.global_conf_path)
         try:
             local_config = cherrypy.lib.reprconf.as_dict(self.local_conf_path)
-            self.__deep_update(self.config, local_config)
+            self.__deep_update(config, local_config)
         except IOError:
             pass
+        self.update(config)
 
-        for key, subkey in self.ABSOLUTIZE_KEYS:
+        for key, subkey in self.ABSOLUTE_PATHS:
             self.__absolutize(key, subkey)
 
         # Manually set tools.staticdir.root from static_root
-        if '/' not in self.config:
-            self.config['/'] = {}
-        self.config['/']['tools.staticdir.root'] = self.static_root
+        if '/' not in self:
+            self['/'] = {}
+        self['/']['tools.staticdir.root'] = self.static_root
 
         # Absolutize any relative filename paths for cherrypy.
-        for key, subdict in self.config.items():
+        for key, subdict in self.items():
             if key[0] == '/':
                 subkey = 'tools.staticfile.filename'
                 if subkey in subdict:
                     self.__absolutize(key, subkey, self.static_root)
+        return config
 
     def __deep_update(self, a, b):
         for k, v in b.items():
@@ -62,13 +116,13 @@ class Config:
         return a
 
     def __absolutize(self, key, subkey, base=None):
-        if key in self.config:
-            if subkey in self.config[key]:
-                path = self.config[key][subkey]
+        if key in self:
+            if subkey in self[key]:
+                path = self[key][subkey]
                 if path[0] != '/':
                     if base is None:
                         base = self.base_dir
-                    self.config[key][subkey] = os.path.join(base, path)
+                    self[key][subkey] = os.path.join(base, path)
 
 # See http://mail.python.org/pipermail/python-ideas/2012-May/014969.html
 sys.modules[__name__] = Config()
--- server.py
+++ server.py
@@ -12,9 +12,9 @@ cherrypy.engine.autoreload.files.add(config.global_conf_path)
 cherrypy.engine.autoreload.files.add(config.local_conf_path)
 
 if __name__ == '__main__':
-    cherrypy.quickstart(root.Root(), config=config.config)
+    cherrypy.quickstart(root.Root(), config=config)
 else:
     # For some reason, this mode requires global settings to be explicitly
     # injected.
-    cherrypy.config.update(config.config['global'])
-    cherrypy.tree.mount(root.Root(), config=config.config)
+    cherrypy.config.update(config['global'])
+    cherrypy.tree.mount(root.Root(), config=config)
