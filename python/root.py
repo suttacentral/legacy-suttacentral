f7b4324dd55eb7cec556b31ec0ea976953822c02
John Nishinaga <jingoro@casa-z.org>
2013-07-03 02:50:22 -0700

    Add 404 page support

--- python/root.py
+++ python/root.py
@@ -1,9 +1,17 @@
-import cherrypy
-import suttacen, show, time
+import cherrypy, os
+import config, suttacen, show, time
 
 # We expose everything we need to here.
 
+def error_page_404(status, message, traceback, version):
+    return open(os.path.join(config.static_root, '404.html'), 'r').read()
+
 class Root(object):
+
+    _cp_config = {
+        'error_page.404': error_page_404
+    }
+
     @cherrypy.expose
     def default(self, *args, **kwargs):
         result = show.dispatch(*args, **kwargs)
