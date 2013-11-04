e62524babb9b35b907999ac310dae295beb5e71a
John Nishinaga <jingoro@casa-z.org>
2013-11-04 10:32:51 -0500

    Intentionally fail a test to test travis email

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -16,3 +16,6 @@ class SimpleTestCase(SCTestCase):
                           self.css('caption').text)
         self.assertEqual('en | vn | en | en | fr | de',
                           self.css('.origin td:last-child').text)
+
+    def test_always_fail(self):
+        self.assertEqual(1, 2)
