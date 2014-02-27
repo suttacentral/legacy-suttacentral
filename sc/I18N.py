68e9bb3f085447fefd0ef8addb96a268ed7d4193
Bhante Jhanarato <jhanarato@gmail.com>
2014-02-27 09:32:04 +0800

    Starting to flesh out the I18N class and accompanying test cases.

--- sc/I18N.py
+++ sc/I18N.py
@@ -1,7 +1,67 @@
+""" Support for internationalization (I18N is the usual abbreviation) 
+
+The I18N class allows access to the data stored in the I18N CSV file.
+The CSV file will have an arbitrary number of columns, the first being
+the key by which localizable elements are identified. The subsequent 
+columns are named according to ISO language conventions. Each language
+has one and only one column (this should be checked!) Some values
+may be missing and defaults provided. So something like this:
+
+    key,en,vn
+    dn1,The All Embracing Net of Views,Kinh Phạm võng
+    dn2,The Fruits of Recluseship,,
+
+This data is slurped up into memory along with the rest of the IMM.
+At this point we ignore issues of performance and memory usage, but
+I'm happy to change things as the design becomes more clear.
+
+USAGE:
+
+>>> from sc import I18N
+>>> localizer = I18N.I18N()
+>>> english = localizer.localize('dn1', 'en')
+>>> print(english)
+The All-embracing Net of Views
+
+"""
+
+import csv
+import sc
+
+from sc.util import ScCsvDialect
+
 class I18N:
     def __init__(self):
-        self.hello = 'Hello'
+        self.i18n_data = {}
+        self.file_name = 'I18N.csv'
 
-    def getHello(self):
-        return self.hello
+    # Open the CSV file containing our localizations
+    # with & as used to make sure the file is unlocked
+    # if an exception is thrown.
+    def read_data(self):
+        with (sc.table_dir / self.file_name).open('r',
+              encoding='utf-8', newline='') as f:
+            reader = csv.reader(f, dialect=ScCsvDialect)
+            field_names = next(reader)
+
+            # This gives us all the column headings
+            for index, field_name in enumerate(field_names):
+                print(str(index) + ' ' + field_name)
+
+            # Each row may contain several translations
+            # We need to pair each translation with its
+            # language code specified in the first row
+            for row in reader:
+                if not any(row): # Drop entirely blank lines
+                    continue
+                if row[0].startswith('#'):
+                    continue
+
+                key = row[0]
+                for index, translation in enumerate(row[1:]):
+                    print(str(index) + ' ' + translation)
+    
+    # Add a language to which translations can be added.
+    def add_language(self, language):
+        self.i18n_data[language] = {}
 
--- tests/unit/test_i18n.py
+++ tests/unit/test_i18n.py
@@ -1,10 +1,18 @@
 import unittest
 import sc
 
-from sc.I18N import I18N
+from sc import I18N
 
 class I18NTest(unittest.TestCase):
-    def test_I18N(self):
-        i18n = I18N() #Testing that we can make one of these
-        self.assertEqual(i18n.getHello(), 'Hello')
+    def test_read_data(self):
+        i18n = I18N.I18N()
+        i18n.file_name = 'test_I18N.csv'
+        i18n.read_data()
+    
+    def test_add_language(self):
+        i18n = I18N.I18N()
+        with self.assertRaises(KeyError):
+            does_not_exist = i18n.i18n_data['en']
+        i18n.add_language('en')
+        now_should_exist = i18n.i18n_data['en']
         
