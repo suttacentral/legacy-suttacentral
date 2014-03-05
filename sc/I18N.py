388758de19c5f1ce7ba3dda073785af1a4132142
Bhante Jhanarato <jhanarato@gmail.com>
2014-03-06 07:55:46 +0800

    Fleshing out I18N class. Now handles non-existent languages and keys. Not fully working yet.

--- sc/I18N.py
+++ sc/I18N.py
@@ -34,25 +34,20 @@ class I18N:
     def __init__(self):
         self.i18n_data = {}
         self.file_name = 'I18N.csv'
+        self.column_names = []
 
     """ Open the CSV file containing our localizations
-    with & as used to make sure the file is unlocked
-    if an exception is thrown. """
+        with & as used to make sure the file is unlocked
+        if an exception is thrown. """
     def read_data(self):
         with (sc.table_dir / self.file_name).open('r',
               encoding='utf-8', newline='') as f:
             reader = csv.reader(f, dialect=ScCsvDialect)
-            column_names = next(reader)
+            # The first line in the CSV file contains 
+            # the column names. Includes key and languages.
+            self.column_names = next(reader)
 
-            # This gives us all the column headings
-            for index, column_name in enumerate(column_names):
-                # TODO Keep the column names in order to 
-                # look them up.
-                print(str(index) + ' ' + column_name)
-
-            # Each row may contain several translations
-            # We need to pair each translation with its
-            # language code specified in the first row
+            # Process each line of data one by one.
             for line in reader:
                 self.add_line(line)
 
@@ -61,13 +56,27 @@ class I18N:
         self.i18n_data[language] = {}
 
     """ Return true if the language has already been
-    added to the i18n_data structure """
+        added to the i18n_data structure """
     def language_exists(self, language):
         return language in self.i18n_data
     
+    """ Given the column number return the language """
+    def get_language(self, column_number):
+        return self.column_names[column_number]
+
     """ Add a translation for a given key and language """
     def add_translation(self, language, key, translation):
         self.i18n_data[language][key] = translation
+
+    """ Given a key and a language, return the translation 
+        If we can't find what we are looking for, return 
+        an empty string. Another way of handling missing data
+        would be to throw an exception. But we don't do that. """
+    def get_translation(self, language, key):
+        if language not in self.i18n_data: return ''
+        if key not in self.i18n_data[language]: return ''
+
+        return self.i18n_data[language][key]
 
     """ Given the data for a single line in the CSV file
     add whatever translations are present in the line
@@ -75,17 +84,18 @@ class I18N:
     def add_line(self, line):
         if not any(line): # Drop entirely blank lines
             return
-        if line[0].startswith('#'):
+        if line[0].startswith('#'): # Drop comment lines
             return
-"""
-        # The code below needs to be enworkened
+        
+        # The key is first in the row.
         key = line[0]
+
         for index, translation in enumerate(line[1:]):
-            language = self.get_language(self, index)
+            language = self.get_language(index)
             if self.language_exists(language):
-                self.add_translation(language, translation)
-            else
+                self.add_translation(language, key, translation)
+            else:
                 self.add_language(language)
-                self.add_translation(language, translation)
-"""                
+                self.add_translation(language, key, translation)
+                
 
--- tests/unit/test_i18n.py
+++ tests/unit/test_i18n.py
@@ -29,9 +29,22 @@ class I18NTest(unittest.TestCase):
         with self.assertRaises(KeyError):
             does_not_exist = i18n.i18n_data['ab']['dn']
         
+    def test_get_translation(self):
+        i18n = I18N.I18N()
+        i18n.add_language('en')
+        i18n.add_translation('en', 'dn', 'Long Discourses')
+        self.assertEqual(i18n.get_translation('en', 'dn'), 'Long Discourses')
+        # No such key - return empty string
+        self.assertEqual(i18n.get_translation('en', '00'), '')
+        # No such language - return empty string
+        self.assertEqual(i18n.get_translation('00', '00'), '')
+        
     def test_language_exists(self):
         i18n = I18N.I18N()
         self.assertFalse(i18n.language_exists('en'))
         i18n.add_language('en')
         self.assertTrue(i18n.language_exists('en'))
-        
+
+    @unittest.skip('\nNot sure how to test this independently.')  
+    def test_get_language(self):
+        i18n = I18N.I18N()
