f89ef6366c7ac4174128353e9d6fa80c7aab8887
Bhante Nandiya <bhante.nandiya@gmail.com>
2014-06-10 11:03:26 +1000

    Menu is closer to working properly.

--- static/js/header_menu.js
+++ static/js/header_menu.js
@@ -9,12 +9,42 @@ sc.headerMenu = {
     toggle: function(e){
         $(this).toggleClass('active');
         $('header nav').not(this).removeClass('active')
+    },
+    update: function(element, mode) {
+        console.log('Hi There!');
+        var self = sc.headerMenu,
+            target = $(element.find('[href]').attr('href'));
+        
+        
+        if (mode === undefined && target.hasClass('active')) {
+            mode = "hide";
+        } else {
+            mode = "show";
+        }
+
+        console.log(mode);
+        console.log(target);
+
+        if (mode == "hide") {
+            self.hideAll();
+        }
+        else {
+            self.hideAll();
+            $('#panel-screen-wrap').show()
+            target.addClass('active');
+        }
+    },
+    hideAll: function(e){
+        $('#panel > .contents').removeClass('active');
+        $('#panel-screen-wrap').hide();
     }
+        
 }
 
 
-$(document).ready(function(){
-    $('header nav').on('hover', sc.headerMenu.activate, sc.headerMenu.deactivate)
-                    .on('click', sc.headerMenu.toggle);
-
-});
+setTimeout(function(){
+    $('header nav').on('click', function(){sc.headerMenu.update($(this));});
+    $('#panel-screen-wrap').on('click', sc.headerMenu.hideAll);
+    //$('#panel').on('click', function(e){e.stopPropagation()});
+    console.log('Okay');
+}, 10);
