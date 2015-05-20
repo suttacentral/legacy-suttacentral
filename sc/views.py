c778e1c7367b1ce4c0ce6d8b6ef5117bf22d57ed
Blake Walsh <blakeofthepath@gmail.com>
2015-05-20 13:14:04 +0300

    Helped reduce the possibility of an exception if something goes wrong with discourse/elasticsearch

--- sc/views.py
+++ sc/views.py
@@ -370,7 +370,11 @@ class TextView(ViewBase):
             query = '%22{}%22|{}'.format(imm.uid_to_acro(self.uid).replace(' ', ' '), self.uid)
             context.discourse_link = '{}?search={}'.format(sc.config.discourse['forum_url'], query)
             context.discourse_url = sc.config.discourse['forum_url']
-            context.discourse_results = sc.search.discourse.search(self.uid)
+            try:
+                context.discourse_results = sc.search.discourse.search(self.uid)
+            except:
+                logger.exception('Failed to retrieve discourse_results')
+                context.discourse_results = None
         if context.embed:
             context.text = self.shorter_text(context.text)
         context.has_quotes = '‘' in context.text or '“' in context.text
