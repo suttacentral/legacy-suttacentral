34744987315380b57bb0782d1777cbe063dd355f
SamaneriVimala <samaneri.vimala@gmail.com>
2015-12-04 20:03:52 +0100

    german dictionary v2 changes

--- static/js/sc_functions.js
+++ static/js/sc_functions.js
@@ -130,7 +130,7 @@ sc.piLookup = {
         {
             lang: 'de',
             name: 'Deutsch',
-            scriptUrl: 'data/pi2de-maindata-v1.js'
+            scriptUrl: 'data/pi2de-maindata-v2.js'
         }, 
         {
             
