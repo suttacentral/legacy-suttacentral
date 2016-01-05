7769307706af3eb933437427b2150fa71966e53f
Blake Walsh <blakeofthepath@gmail.com>
2016-01-05 17:19:59 +0200

    pointless improvements

--- static/js/lib/idb-cache.js
+++ static/js/lib/idb-cache.js
@@ -4,6 +4,7 @@
 
 function IDBCache(name, version){
     this.name = name;
+    this.dbname = '_idb_cache_' + name;
     this.version = version;
     this.available = false;
     this.open();
@@ -12,30 +13,32 @@ function IDBCache(name, version){
 IDBCache.prototype = {
     open: function() {
         var self = this,
-            version = this.version,
-            name = this.name;
+            version = this.version;
             
         var promise = new Promise(function(resolve, reject){
             //Opening the DB
-            var request = indexedDB.open(name + "Data", version);      
+            var request = indexedDB.open(self.dbname, version);      
+            console.log('Opening Database named ' + self.dbname);
      
             //Handling onupgradeneeded
             //Will be called if the database is new or the version is modified
             request.onupgradeneeded = function(e) {
+                console.log('Update Needed')
                 var db = e.target.result;
                 e.target.transaction.onerror = indexedDB.onerror;
      
                 //Deleting DB if already exists
-                if(db.objectStoreNames.contains(name)) {
-                    db.deleteObjectStore(name);
+                if(db.objectStoreNames.contains(self.name)) {
+                    db.deleteObjectStore(self.name);
                 }
                 //Creating a new DB store with a paecified key property
-                var store = db.createObjectStore(name,
+                var store = db.createObjectStore(self.name,
                     {keyPath: "id"});
             };
      
             //If opening DB succeeds
             request.onsuccess = function(e) {
+                console.log('Success opening Database')
                 self.db = e.target.result;
                 self.available = true;
                 resolve();
@@ -43,11 +46,24 @@ IDBCache.prototype = {
      
             //If DB couldn't be opened for some reason
             request.onerror = function(e){
+                console.log('Error opening Database')
                 reject("Couldn't open DB");
             };
         });
         return promise;
     },
+    destroy: function() {
+        var promise = new Promise(function(resolve, reject) {
+            var req = indexedDB.deleteDatabase(this.dbname);
+            req.onsuccess = resolve
+            req.onerror = reject
+            req.onblocked = function() {
+                console.log("Delete Operation Blocked");
+                reject();
+            }
+        })
+        return promise;
+    },        
     add: function(key, value) {
         //Creating a transaction object to perform read-write operations
         var store = this.db.transaction([this.name], "readwrite").objectStore(this.name);
